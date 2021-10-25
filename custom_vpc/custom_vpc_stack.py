ea8cdf6f4755f06c26a11d011c41a7e78348a44a
Chanky3 <c.innovator@gmail.com>
2021-10-25 18:19:12 +0530

    modified few files

--- custom_vpc/custom_vpc_stack.py
+++ custom_vpc/custom_vpc_stack.py
@@ -1,15 +1,24 @@
-from aws_cdk import core as cdk
-
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
+from aws_cdk import (
+            aws_ec2 as ec2,
+                core
+                )
 
 
-class CustomVpcStack(cdk.Stack):
+class CustomVpcStack(core.Stack):
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+            super().__init__(scope, id, **kwargs)
 
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+                            # The code that defines your stack goes here
+                            # https://docs.aws.amazon.com/cdk/api/latest/python/aws_cdk.aws_ec2/Vpc.html
+            vpc = ec2.Vpc(
+                    self, "MyVpc",
+                    cidr="10.13.0.0/21",
+                    max_azs=2,
+                    nat_gateways=0,
+                    subnet_configuration=[
+                       ec2.SubnetConfiguration(name="public", cidr_mask=24, subnet_type=ec2.SubnetType.PUBLIC),                                                                             # ec2.SubnetConfiguration(name="private", cidr_mask=24, subnet_type=ec2.SubnetType.PRIVATE)                                                                          ec2.SubnetConfiguration(name="private", cidr_mask=24, subnet_type=ec2.SubnetType.ISOLATED)
+                                                    ]
+                                                )
+            # Tag all VPC Resources
+            core.Tag.add(vpc,key="Owner",value="Chanky",include_resource_types=[])
 
-        # The code that defines your stack goes here
